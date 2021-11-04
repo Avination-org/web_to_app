41b5b6a030551568eab8e7feb72efbbcfe36bc1e
KuldeepSingh <kul8003995609deep@gmail.com>
2021-11-04 09:14:30 +0530

    correted teh url of avination.in

--- lib/src/web_page.dart
+++ lib/src/web_page.dart
@@ -24,7 +24,7 @@ class _WebPageViewState extends State<WebPageView> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: WebView(
-        initialUrl: 'https://www.avination.in',
+        initialUrl: 'https://avination.in/',
         javascriptMode: JavascriptMode.unrestricted,
         allowsInlineMediaPlayback: true,
         debuggingEnabled: true,
